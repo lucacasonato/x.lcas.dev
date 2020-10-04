7c08029b9a4e50a1c1471acf177299fc12985289
Luca Casonato <lucacasonato@yahoo.com>
2020-10-04 19:58:18 +0200

    autocomplete

--- server.ts
+++ server.ts
@@ -7,15 +7,32 @@ const root: SendOptions = { root: "public/" };
 
 const router = new Router();
 
+// Homepage
 router.get("/", async (ctx) => {
   await ctx.send({ ...root, path: "./index.html" });
 });
+
+// VS Code autocomplete
 router.get("/.well-known/deno-import-intellisense.json", async (ctx) => {
   await ctx.send(
     { ...root, path: "./.well-known/deno-import-intellisense.json" },
   );
 });
+router.get("/_vsc/m", (ctx) => {
+  ctx.response.headers.set("cache-control", "max-age=3600");
+  ctx.response.body = Object.keys(manifest);
+});
+router.get("/_vsc/m/:module", (ctx) => {
+  if (!ctx.params.module || !manifest[ctx.params.module]) {
+    ctx.response.status = 404;
+    ctx.response.body = "not found";
+    return;
+  }
+  ctx.response.headers.set("cache-control", "max-age=3600");
+  ctx.response.body = Object.keys(manifest[ctx.params.module]);
+});
 
+// Registry
 router.get("/:module/:path+", async (ctx) => {
   const { module, path } = ctx.params;
   if (!module || !path) return;
@@ -32,19 +49,6 @@ router.get("/:module/:path+", async (ctx) => {
     );
   }
   await ctx.send({ ...root, path: `/${module}/${path}` });
-});
-
-// VS Code autocomplete
-router.get("/_vsc", (ctx) => {
-  ctx.response.body = Object.keys(manifest);
-});
-router.get("/_vsc/m/:module", (ctx) => {
-  if (!ctx.params.module || !manifest[ctx.params.module]) {
-    ctx.response.status = 404;
-    ctx.response.body = "not found";
-    return;
-  }
-  ctx.response.body = Object.keys(manifest[ctx.params.module]);
 });
 
 const app = new Application();
