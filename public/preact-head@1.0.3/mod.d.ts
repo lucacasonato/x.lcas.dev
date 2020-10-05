bf427766dd077c0b5eb6ba3077c7e48d21d9d11b
Luca Casonato <lucacasonato@yahoo.com>
2020-10-05 12:43:45 +0200

    fix preact-head@1.0.3 d.ts

--- public/preact-head@1.0.3/mod.d.ts
+++ public/preact-head@1.0.3/mod.d.ts
@@ -1,5 +1,6 @@
-import { Component, JSX } from "../preact@10.5.3/mod.d.ts";
+import { Component, ComponentChild, JSX } from "../preact@10.5.3/mod.d.ts";
 
 export default class Head extends Component {
   public static rewind(): JSX.Element;
+  render(): ComponentChild;
 }
