3d1256133f32532398892155653ee07aa8584705
Luca Casonato <lucacasonato@yahoo.com>
2020-12-02 16:56:22 +0100

    preact@10.5.7

---
+++ public/preact@10.5.7/mod.js
@@ -0,0 +1,1 @@
+export * from "https://cdn.skypack.dev/pin/preact@v10.5.7-xX7vRkUFu3YZatNYMF7b/preact.js";
---
+++ public/preact@10.5.7/ssr.js
@@ -0,0 +1,1 @@
+export * from "https://cdn.skypack.dev/pin/preact-render-to-string@v5.1.12-z1KL3yYcMlBfwpVusZgg/preact-render-to-string.js";
